7fefcced6569fa97ab7153c776ec36fe82588398
Amanda Jensen <jens2482@umn.edu>
2024-04-05 18:03:03 -0500

    cluster version created and cleaned of comments

--- R/week11-cluster.R
+++ R/week11-cluster.R
@@ -3,9 +3,9 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(haven)
 library(caret)
-library(parallel) #added this library for parallelization
-library(doParallel) #added this library for parallelization
-library(tictoc) #added this library to track time
+library(parallel) 
+library(doParallel)
+library(tictoc) 
 
 # Data Import and Cleaning
 data = read_sav("../data/GSS2016.sav")
@@ -29,10 +29,10 @@ set.seed(54321)
 rows <- sample(nrow(gss_tbl))
 shuffled_data <-  gss_tbl[rows,]
 split <- round(nrow(gss_tbl)*0.75)
-train_gss_tbl <- as.data.frame(shuffled_data[1:split,]) #added as.data.frame because otherwise my createFolds wouldn't work
-test_gss_tbl <- as.data.frame(shuffled_data[(split+1):nrow(gss_tbl),] ) #made this one a data frame too for consistency
+train_gss_tbl <- as.data.frame(shuffled_data[1:split,]) 
+test_gss_tbl <- as.data.frame(shuffled_data[(split+1):nrow(gss_tbl),] ) 
 
-training_folds <- createFolds(train_gss_tbl$work_hours, 10) #didn't have this in my original code, but you said that we needed to do this in your video
+training_folds <- createFolds(train_gss_tbl$work_hours, 10) 
 
 myControl <- trainControl( 
   method = "cv", 
@@ -41,7 +41,7 @@ myControl <- trainControl(
   indexOut = training_folds
 )
 
-#models without parallelization
+
 tic() #start time
 model_lm <- train( 
   work_hours ~ ., 
@@ -56,9 +56,9 @@ p_lm <- predict(model_lm,
                 test_gss_tbl, 
                 na.action = na.pass)
 p_lm_results <- cor(p_lm, as.data.frame(test_gss_tbl)$work_hours)^2 
-toc_lm <- toc() #end time
+toc_lm <- toc() 
 
-tic() #start time
+tic() 
 model_enet <- train( 
   work_hours ~ ., 
   train_gss_tbl,
@@ -74,7 +74,7 @@ p_enet <- predict(model_enet,
 p_enet_results <- cor(p_lm, as.data.frame(test_gss_tbl)$work_hours)^2
 toc_enet <- toc() #end time
 
-tic() #start time
+tic() 
 model_rf <- train( 
   work_hours ~ .,  
   train_gss_tbl,
@@ -88,9 +88,9 @@ p_rf <- predict(model_rf,
                 test_gss_tbl,
                 na.action = na.pass)
 p_rf_results <- cor(p_rf, as.data.frame(test_gss_tbl)$work_hours)^2
-toc_rf <- toc() #end time
+toc_rf <- toc() 
 
-tic() #start time
+tic() 
 model_xgb <- train( 
   work_hours ~ ., 
   train_gss_tbl,
@@ -104,18 +104,18 @@ p_xgb <- predict(model_xgb,
                   test_gss_tbl,
                   na.action = na.pass)
 p_xgb_results <- cor(p_xgb, as.data.frame(test_gss_tbl)$work_hours)^2
-toc_xgb <- toc() #end time
+toc_xgb <- toc() 
 
-#Models with parallelization
-local_cluster <- makeCluster(detectCores() - 1) #make cluster and set the number of cores to 1 less than number on device
+
+local_cluster <- makeCluster(detectCores() - 1) 
 registerDoParallel(local_cluster)
 
-tic() #start time
+tic() 
 model_lm <- train( 
   work_hours ~ ., 
   train_gss_tbl,
   method = "lm", 
-  preProcess = c("center", "scale", "medianImpute", "nzv"), #added center and scale
+  preProcess = c("center", "scale", "medianImpute", "nzv"), 
   na.action = na.pass, 
   trControl = myControl
 )
@@ -124,9 +124,9 @@ p_lm <- predict(model_lm,
                 test_gss_tbl, 
                 na.action = na.pass)
 p_lm_results <- cor(p_lm, as.data.frame(test_gss_tbl)$work_hours)^2 
-toc_lm_parallel <- toc() #end time
+toc_lm_parallel <- toc() 
 
-tic() #start time
+tic() 
 model_enet <- train( 
   work_hours ~ ., 
   train_gss_tbl,
@@ -140,9 +140,9 @@ p_enet <- predict(model_enet,
                   test_gss_tbl,
                   na.action = na.pass)
 p_enet_results <- cor(p_lm, as.data.frame(test_gss_tbl)$work_hours)^2
-toc_enet_parallel <- toc() #end time
+toc_enet_parallel <- toc()
 
-tic() #start time
+tic() 
 model_rf <- train( 
   work_hours ~ .,  
   train_gss_tbl,
@@ -156,9 +156,9 @@ p_rf <- predict(model_rf,
                 test_gss_tbl,
                 na.action = na.pass)
 p_rf_results <- cor(p_rf, as.data.frame(test_gss_tbl)$work_hours)^2
-toc_rf_parallel <- toc() #end time
+toc_rf_parallel <- toc()
 
-tic() #start time
+tic() 
 model_xgb <- train( 
   work_hours ~ ., 
   train_gss_tbl,
@@ -172,7 +172,7 @@ p_xgb <- predict(model_xgb,
                  test_gss_tbl,
                  na.action = na.pass)
 p_xgb_results <- cor(p_xgb, as.data.frame(test_gss_tbl)$work_hours)^2
-toc_xgb_parallel <- toc() #end time
+toc_xgb_parallel <- toc() 
 
 stopCluster(local_cluster) #stop cluster
 
@@ -193,14 +193,8 @@ table1_tbl <- tibble(
 )
 
 table2_tbl <- tibble( 
-  algo = c("OLS Regression", "Elastic Net", "Random Forest", "eXtreme Gradient Boosting"), #add row labels for each model
-  original = c(toc_lm$callback_msg, toc_enet$callback_msg, toc_rf$callback_msg, toc_xgb$callback_msg), #pull time amounts from toc for non-parallelized
-  parallelized = c(toc_lm_parallel$callback_msg, toc_enet_parallel$callback_msg, toc_rf_parallel$callback_msg, toc_xgb_parallel$callback_msg) #pull time amounts from toc for parallelized
+  algo = c("OLS Regression", "Elastic Net", "Random Forest", "eXtreme Gradient Boosting"),
+  original = c(toc_lm$callback_msg, toc_enet$callback_msg, toc_rf$callback_msg, toc_xgb$callback_msg),
+  parallelized = c(toc_lm_parallel$callback_msg, toc_enet_parallel$callback_msg, toc_rf_parallel$callback_msg, toc_xgb_parallel$callback_msg)
 )
 
-#Which models benefited most from parallelization and why?
-  #eXtreme Gradient Boosting benefited the most from parallelization (a rougly 32% decrease in time, with the next closest being random forest with a 27% decrease) because it is the most complex and had the most combinations of different hyperparameters to test. 
-#How big was the difference between the fastest and slowest parallelized model? Why?
-  #There were 198.54 seconds between the fastest and slowest parallelized models. That is a smaller spread that with the non-parallelized models (300.6 seconds). So clearly parallelization helps to improve the speed, especially with more complex models. However, the simpler models can only go so much faster than they did in the first place so the time doesn't change much for those and they continue to run quickly.
-#If your supervisor asked you to pick a model for use in a production model, which would you recommend and why? Consider both Table 1 and Table 2 when providing an answer.
-  #Similar to the last assignment, I would still choose Random Forest. It has the highest R2 value between all the models and also runs a good amount faster than eXtreme gradient boosting both when parallelizing and not.
